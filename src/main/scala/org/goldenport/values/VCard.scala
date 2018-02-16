3ed147e48c78be3664f539c6ae9aa3ed8e5120bd
ASAMI Tomoharu <asami.tomoharu@gmail.com>
2018-02-16 10:08:55 +0900

    Update VCard.

--- src/main/scala/org/goldenport/values/VCard.scala
+++ src/main/scala/org/goldenport/values/VCard.scala
@@ -11,7 +11,8 @@ import org.goldenport.Strings
  * 
  * @since   Nov. 18, 2017
  *  version Nov. 24, 2017
- * @version Dec. 13, 2017
+ *  version Dec. 13, 2017
+ * @version Feb. 14, 2018
  * @author  ASAMI, Tomoharu
  */
 case class VCard(
@@ -59,28 +60,35 @@ case class VPhoto(
 //      Mail Drop: TNE QB\n123 Main Street\nAny Town, CA  91921-1234\n
 //      U.S.A.":;;123 Main Street;Any Town;CA;91921-1234;U.S.A.
 case class VAddress(
-  param: Option[VParameters],
-  pobox: Option[String],
-  ext: Option[String],
-  street: Option[String],
-  locality: Option[String], // city
-  region: Option[String], // state or province
+  country: Option[String],
   code: Option[String],
-  country: Option[String]
+  region: Option[String], // state or province
+  locality: Option[String], // city
+  street: Option[String],
+  ext: Option[String],
+  pobox: Option[String],
+  supplement: Option[String],
+  properties: Map[String, String]
+//  param: Option[VParameters],
 )
 
-case class VTel(value: String)
+case class VTel(
 
-case class VEmail(value: String)
+  value: String
+)
+
+case class VEmail(
+  value: String
+)
 
 case class VRevision(value: String)
 
-case class VParameters(
-  label: Option[VLabel],
-//  geo: Option[VGeo],
-//  language: Option[VLanguage],
-//  tz: Option[VTz],
-  pref: Option[Int] // 0...100
-)
+// case class VParameters(
+//   label: Option[VLabel],
+// //  geo: Option[VGeo],
+// //  language: Option[VLanguage],
+// //  tz: Option[VTz],
+//   pref: Option[Int] // 0...100
+// )
 
 case class VLabel(value: String)
