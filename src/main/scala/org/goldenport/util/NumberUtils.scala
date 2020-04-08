81f31e60cf691fac948f8c83f14bc647d939d409
ASAMI Tomoharu <asami.tomoharu@gmail.com>
2020-04-08 11:16:05 +0900

    Update VectorMap and NumberUtils.

--- src/main/scala/org/goldenport/util/NumberUtils.scala
+++ src/main/scala/org/goldenport/util/NumberUtils.scala
@@ -2,7 +2,7 @@ package org.goldenport.util
 
 /*
  * @since   Mar. 12, 2020
- * @version Mar. 12, 2020
+ * @version Apr.  3, 2020
  * @author  ASAMI, Tomoharu
  */
 object NumberUtils {
@@ -12,6 +12,18 @@ object NumberUtils {
     case "true" => Some(true)
     case "false" => Some(false)
     case _ => None
+  }
+
+  def getInt(p: String): Option[Int] = try {
+    Some(p.trim.toInt)
+  } catch {
+    case e: NumberFormatException => None
+  }
+
+  def getLong(p: String): Option[Long] = try {
+    Some(p.trim.toLong)
+  } catch {
+    case e: NumberFormatException => None
   }
 }
 
